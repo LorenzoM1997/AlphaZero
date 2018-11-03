0f8b9811914464da586449bfc7dd8bd0b1603260
Justin Wang <jtwwang@ucdavis.edu>
2018-11-03 02:36:49 -0700

    Added documentation for class vars and implemented _build_value_head()

--- nn.py
+++ nn.py
@@ -4,12 +4,17 @@ import tensorflow as tf
 import numpy as np
 
 class NN():
-	def __init__(self, lr, input_dim, num_hidden_layers, filters, kernelsize, strides=1, padding="same"):
+	def __init__(self, lr, input_dim, num_hidden_layers, value_head_dim, policy_head_dim, filters, kernelsize, training, strides=1, padding="same"):
 		""" 
 		Args:
-			lr (float): learning rate
-			input_dim (int list): length, height, layers of input
-			num_hidden_layers (int): number of hidden layers
+			lr (float): Learning rate
+			input_dim (int tuple/list): Length, height, layers of input
+			num_hidden_layers (int): Number of hidden layers
+			filters (int): num features in output of convolution
+			kernelsize (int tuple/list or int): Size of convolving window
+			training (bool): True if model is training
+			strides (int tuple/list or int): Stride of convolution
+			padding ("same" or "valid"): "same" if 0 adding added during convolution
 		"""
 		self.lr = lr
 		self.input_dim = input_dim
@@ -18,8 +23,9 @@ class NN():
 		self.kernelsize = kernelsize
 		self.strides = strides
 		self.padding = padding
+		self.training = training
 
-		self.inputs = tf.placeholder(shape=input_dim,dtype=tf.float32)
+		self.inputs = tf.placeholder(shape=np.append(None,input_dim),dtype=tf.float32) # Variable batch size
 		self.hidden_layers = _build_hidden_layers()
 		self.value_head = _build_value_head()
 		self.policy_head = _build_policy_head()
@@ -30,17 +36,33 @@ class NN():
 			List of resBlocks
 		"""
 		hidden_layers = []
-		resblk = resBlock(self.inputs, self.filters, self.kernelsize, self.strides, self.padding, True)
+		resblk = resBlock(self.inputs, self.filters, self.kernelsize, True, self.strides, self.padding)
 		hidden_layers.append(resblk)
 		if num_hidden_layers > 1:
 			for i in range(num_hidden_layers-1):
-				resblk = resBlock(resblk, self.filters, self.kernelsize, self.strides, self.padding, True)
+				resblk = resBlock(resblk, self.filters, self.kernelsize, True, self.strides, self.padding)
 				hidden_layers.append(resblk)
 		return hidden_layers
 
-	def _build_value_head():
-		#value_head = conv2d(self.hidden_layers[-1], self.kernelsize,)
-		return None
+	def _build_value_head(self):
+		vh = conv2d(self.hidden_layers[-1], (1,1), 1, "same", None)
+		vh_bn = batch_norm(vh, self.training)
+		vh_bn_relu = tf.nn.relu(vh_bn)
+		vh_flat = tf.layers.flatten(vh_bn_relu)
+		vh_dense = tf.layers.dense(
+			inputs=vh_flat,
+			units=20,
+			use_bias=False,
+			activation=tf.nn.leaky_relu
+		)
+		vh_out = tf.layers.dense(
+			inputs=vh_dense,
+			units=1,
+			use_bias=False,
+			activation=tf.nn.tanh
+		)
+
+		return vh_out
 
 	def _build_policy_head():
 		return None
@@ -58,7 +80,7 @@ class NN():
 	def batch_norm(inputs, training, BATCH_MOMENTUM = 0.997, BATCH_EPSILON = 1e-5):
 		return tf.layers.batch_normalization(
 			inputs=inputs, 
-			axis=-1, 
+			axis=1, 
 			momentum=_BATCH_NORM_DECAY, 
 			epsilon=_BATCH_NORM_EPSILON, 
 			center=True, 
