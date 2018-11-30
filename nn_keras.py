b26545cdf132ce737007699e46ef61f8b56a130b
Daniel Loran <dcloran@ucdavis.edu>
2018-11-29 22:17:32 -0800

    policy_head added

--- nn_keras.py
+++ nn_keras.py
@@ -5,3 +5,31 @@ import numpy as np
 import math
 import os
 import shutil
+
+
+
+def policy_head(self, x):
+
+	x = Conv2D(
+	filters = 2
+	, kernel_size = (1)
+	, data_format="channels_first"
+	, padding = 'same'
+	, use_bias=False
+	, activation='linear'
+	, kernel_regularizer = regularizers.l2(self.reg_const)
+	)(x)
+
+	x = LeakyReLU()(x)
+
+	x = Flatten()(x)
+
+	x = Dense(
+		self.output_dim
+		, use_bias=False
+		, activation='linear'
+		, kernel_regularizer=regularizers.l2(self.reg_const)
+		, name = 'policy_head'
+		)(x)
+
+    return (x)
