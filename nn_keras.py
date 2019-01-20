a0227e35e6155223fcb24fbf6cd17bd5d8da54b0
Lorenzo Mambretti <lmambretti@ucdavis.edu>
2019-01-20 12:51:47 -0800

    new model in keras

--- nn_keras.py
+++ nn_keras.py
@@ -1,98 +1,109 @@
-	# keras implementation of nn.py
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.backend as K
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, DepthwiseConv2D, SeparableConv2D
+from tensorflow.keras.layers import Flatten, MaxPool2D, AvgPool2D, GlobalAvgPool2D, UpSampling2D
+from tensorflow.keras.layers import BatchNormalization, concatenate, add, Dropout, ReLU, Lambda, Activation, LeakyReLU
 
-from keras.layers import Conv2D, LeakyReLU
-from keras.engine.input_layer import Input
+from IPython.display import SVG
+#from keras.utils.vis_utils import model_to_dot
+
+from time import time
 import numpy as np
-import math
-import os
-import shutil
 
 class NN():
-	def __init__(self, input_dim, num_hidden_layers, policy_head_dim, lr=0.00025, kernel_size = 1, strides = 1):
-		self.input_dim = input_dim
-		self.model = self._create_model()
+    def __init__(self, input_shape, num_hidden_layers, policy_head_dim, lr=0.1, kernel_size=1, filters=32, strides=1, padding="same"):
 
-	def _conv2d(self, x, filters, kernel_size, kernel_regularizer):
-		x = Conv2D(
-			filters=filters,
-			kernel_size = kernel_size,
-			activation = 'linear',
-			kernel_regularizer = kernel_regularizer,
-			data_format= 'channels_first',
-			padding = 'same',
-			use_bias= False
-		)(x)
+        # parameters
+        self.kernel_size = kernel_size
+        self.filters = filters
+        self.strides = strides
 
-		x = LeakyReLU() (x) # activation is not used
+        # model
+        self.input = Input(input_shape)
+  
+        self.c1 = self.conv_bn_rl(self.input, filters, kernel_size)
+        self.maxp = MaxPool2D(3, strides=2, padding='same')(self.c1)
+  
+        self.res1 = self.resnet_block(self.maxp, filters, filters, 2)
+        self.hidden_layers = []
+        self.hidden_layers.append(self.res1)
+        for i in range(num_hidden_layers - 1):
+            x = self.resnet_block(self.hidden_layers[-1], filters, filters, 2)
+            self.hidden_layers.append(x)
+  
+        self.flat = Flatten()(self.hidden_layers[-1])
+  
+        self.policy_head = Dense(policy_head_dim, activation='softmax')(self.flat)
+        self.value_head = Dense(1, activation='tanh')(self.flat)
 
-		return x
+        self.model = Model(inputs = self.input, outputs = [self.policy_head, self.value_head])
 
-	def _create_model(self):
-		""" reteurns the result of one convolution
-		"""
-		input_layer = Input(shape=self.input_dim, name='input_layer')
-		x = self._conv2d(input_layer, 32, 2, LeakyReLU(), None)
-		return x
+        #loss
+        self.model.compile(optimizer='sgd', loss = self.loss_function)
 
-	def policy_head(self, x):
+    def conv_bn_rl(self, x, f, k=1, s=1, p='same'):
+        x = Conv2D(f, k, strides=s, padding=p)(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        return x
+  
+  
+    def conv_block(self, tensor, f1, f2, s):
+        x = self.conv_bn_rl(tensor, f1)
+        x = self.conv_bn_rl(x, f1, 3, s=s)
+        x = Conv2D(f2, 1)(x)
+        x = BatchNormalization()(x)
+  
+        shortcut = Conv2D(f2, 1, strides=s, padding='same')(tensor)
+        shortcut = BatchNormalization()(shortcut)
+    
+        x = add([shortcut, x])
+        output = ReLU()(x)
+    
+        return output
+  
+  
+    def identity_block(self, tensor, f1, f2):
+        x = self.conv_bn_rl(tensor, f1)
+        x = self.conv_bn_rl(x, f1, 3)
+        x = Conv2D(f2, 1)(x)
+        x = BatchNormalization()(x)
+    
+        x = add([tensor, x])
+        output = ReLU()(x)
+    
+        return output
+  
+  
+    def resnet_block(self, x, f1, f2, r):
+        x = self.conv_block(x, f1, f2, self.strides)
+    
+        for _ in range(r-1):
+            x = self.identity_block(x, f1, f2)
+    
+        return x
+  
 
-    def policy_head(self, x):
+    def loss_function(self, yTrue, yPred):
 
-    	x = _conv2d(x, 2, 1, 'linear',
-    	filters = 2
-    	, kernel_size = (1)
-    	, data_format="channels_first"
-    	, padding = 'same'
-    	, use_bias=False
-    	, activation='linear'
-    	, kernel_regularizer = regularizers.l2(self.reg_const)
-    	)(x)
+        policy_label = K.softmax((yTrue[0] + 1) * 0.5) # normalize policy labels
+        value_label = yTrue[1]
+        policy_head = yPred[0]
+        value_head = yPred[1]
 
-    	x = LeakyReLU()(x)
+        cross_entropy = keras.losses.categorical_crossentropy(policy_label, policy_head)
+        mse = keras.losses.mean_squared_error(value_label, value_head)
+        loss = K.sum(cross_entropy + mse)
+        return loss
 
-    	x = Flatten()(x)
+    #def pre_run(self, model_path='/model1/'):
 
-    	x = Dense(
-    		self.output_dim
-    		, use_bias=False
-    		, activation='linear'
-    		, kernel_regularizer=regularizers.l2(self.reg_const)
-    		, name = 'policy_head'
-    		)(x)
+    def fit(self, X, v_lab, p_lab, batch_size=100, epochs=1000, model_saver_path='/model1/'):
+        p_lab = K.softmax((p_lab + 1) * 0.5)
+        self.model.fit(X, [p_lab, v_lab], epochs=epochs, batch_size=batch_size)
 
-        return (x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		x = LeakyReLU()(x)
-
-		x = Flatten()(x)
-
-		x = Dense(
-			self.output_dim
-			, use_bias=False
-			, activation='linear'
-			, kernel_regularizer=regularizers.l2(self.reg_const)
-			, name = 'policy_head'
-			)(x)
-
-		return (x)
-
-if __name__ == "__main__":
-	nn = NN((2,3,3),2,8)
+    def pred(self, X):
+        prediction = self.model.predict(X)
+        print(prediction)
